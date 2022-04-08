48f1d4c9ef81e67e3ff974fff49f479610e47fe9
Daniel Bajka <daniel.bajka@gmail.com>
2022-04-08 13:59:44 +0200

    Add input validation
    In Zeile 12 wird der Eingabeparameter $1 ohne Validierung übernommen. 
    $1 steht für die Anzahl zu generierender Mac-Adressen. 
    Eingaben dürfen nur Zahlen sein.
    Ein sinnvolles Wertebereich liegt zwischen 1 und 99 Mac-Adressen.

--- tools/create-hwmac.sh
+++ tools/create-hwmac.sh
@@ -8,27 +8,38 @@
 # - The first Parameter is the number of addresses to generate 
 # - The second parameter is the vendor-prefix for the addresses in 0X-0X-0X format
 
+# ask for user input and store it in a variable
+read -p 'How many mac-addresses shall be generated: ' N_ADDRESSES
 
-N_ADDRESSES=$1
+# regular expression the user input is tested against
+re='^([[:digit:]]{0,2})$'
 
-# The default prefix is the linux XEN vendor format as it is used also by 
-#    linux for virtual hardware
-MAC_PREFIX='00-16-3e'
+# comparing the user input to regex
+# if true start the calculation of mac addresses 
+if [[ $N_ADDRESSES =~ $re ]]
+then 
+    # The default prefix is the linux XEN vendor format as it is used also by 
+    #    linux for virtual hardware
+    MAC_PREFIX='00-16-3e'
 
-if [ "x$N_ADDRESSES" = "x" ]
-then
-    N_ADDRESSES=1
+    if [ "x$N_ADDRESSES" = "x" ]
+    then
+        N_ADDRESSES=1
+    fi
+
+    # If a second parameter is passed, then use it as MAC-prefix
+    if [ "x$2" != "x" ]
+    then
+        MAC_PREFIX=$2
+    fi
+
+    MACTEMPLATE="$MAC_PREFIX-%02X-%02X-%02X\n"
+
+    for X in `seq 1 $N_ADDRESSES`
+    do
+        printf "  - macaddress: $MACTEMPLATE" $[RANDOM%256] $[RANDOM%256] $[RANDOM%256]
+    done
+# if the user input is invalid print a comment
+else
+    echo "Please enter a number ..."
 fi
-
-# If a second parameter is passed, then use it as MAC-prefix
-if [ "x$2" != "x" ]
-then
-    MAC_PREFIX=$2
-fi
-
-MACTEMPLATE="$MAC_PREFIX-%02X-%02X-%02X\n"
-
-for X in `seq 1 $N_ADDRESSES`
-do
-    printf "  - macaddress: $MACTEMPLATE" $[RANDOM%256] $[RANDOM%256] $[RANDOM%256]
-done
