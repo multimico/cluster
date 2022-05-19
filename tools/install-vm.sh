a8e18ad631a166ce7b9e2bb5d7633f953600c1c8
Christian Glahn <cpglahn@gmail.com>
2022-05-19 16:14:55 +0200

    echos

--- tools/install-vm.sh
+++ tools/install-vm.sh
@@ -66,7 +66,7 @@ CIDATA=$(cat $CLOUD_INIT | envsubst )
 # echo $CIDATA | lxc config set $HOSTNAME user.user-data -
 
 echo $PROFILE $MACADDRESS
-echo $CIDATA
+echo -e $CIDATA
 
 # lxc config set $HOSTNAME volatile.eth0.hwaddr $MACADDRESS
 
