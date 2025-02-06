899460f9485dfe6f7db3cf75ec0119bdba48c6be
Christian Glahn <glah@zhaw.ch>
2025-02-06 15:18:03 +0100

    add changes to more flexible vm creation

--- tools/install-instance.sh
+++ tools/install-instance.sh
@@ -8,59 +8,60 @@
 
 # CONSTANTS NOT TO CUSTOMISE
 # DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-TDIR=/home/multimico/src/multimico-cluster
-CDIR=/home/multimico/src/cluster-config
+# TDIR=/home/multimico/src/multimico-cluster
+# CDIR=/home/multimico/src/cluster-config
 
-HYPERVISOR=$( hostname )
+# HYPERVISOR=$( hostname )
 
-# HOSTNAME=`petname`
+HOSTNAME=$(petname)
 
 # Variables
 # TODO: We want to customise the image
 OSNAME=ubuntu
 OSVERSION="24.04"
 
-# get the code name for the docker repos
-OSVERSIONNAME=$(osinfo-query os short-id=${OSNAME}${OSVERSION} -f codename | tail -n 1 | sed -E "s/^\\s*(\\w+).*/\\L\\1/")
-# TODO: We my want to customise the cloud_init file.
 CLOUD_INIT=${CDIR}/profiles/cloud_init.cfg
 
 # Parameters
-HOSTNAME=$1
+# HOSTNAME=$1
 
 # TODO: The username and the GH names should be configurable
-USERNAME=$2
-GITHUBNAME=$3
+USERNAME=$1
+GITHUBNAME=$2
 
 # MACADDRESS=$( yq ".nodes[] | select(.name == \"${HOSTNAME}\").macaddress" "${CDIR}/nodes/hardware_macs.yaml" )
 # PROFILE=$( yq ".nodes[] | select(.name == \"${HOSTNAME}\").profile" "${CDIR}/nodes/hardware_macs.yaml" )
 # INTENT_HOST=$( yq ".nodes[] | select(.name == \"${HOSTNAME}\").host" "${CDIR}/nodes/hardware_macs.yaml" )
 
-if [ $HYPERVISOR != $INTENT_HOST ]
-then
-    echo "Node is not intendend for this host"
-    exit 0
-fi
+# if [ $HYPERVISOR != $INTENT_HOST ]
+# then
+#     echo "Node is not intendend for this host"
+#     exit 0
+# fi
 
-IS_RUNNING=$(lxc list --format=yaml | yq ".[] | select(.name == \"${HOSTNAME}\").state.status")
+# IS_RUNNING=$(lxc list --format=yaml | yq ".[] | select(.name == \"${HOSTNAME}\").state.status")
 
-if [ "$IS_RUNNING" == "Running" ]
-then
-    echo "Node is already running. Avoid restarting it!"
-    exit 0
-fi
+# if [ "$IS_RUNNING" == "Running" ]
+# then
+#     echo "Node is already running. Avoid restarting it!"
+#     exit 0
+# fi
 
 # MACADDRESS=$(echo $MACADDRESS | sed -E s/-/:/g | sed 's/.*/\L&/' )
+
+read -p "which profile you want to use (use incus profile list for available profiles): " PROFILE
+echo
+
 
 if [ -z $PROFILE ]
 then
     PROFILE=default
 fi
 
-if [ $PROFILE != "default" ]
-then
-    CLOUD_INIT=${CLOUD_INIT}_${PROFILE}
-fi
+# if [ $PROFILE != "default" ]
+# then
+#     CLOUD_INIT=${CLOUD_INIT}_${PROFILE}
+# fi
 
 # echo "The new host is called '$HOSTNAME'"
 
@@ -100,8 +101,6 @@ fi
 # read -p "Memory Limit in GB (empty for no limit): " MEMLIMIT
 # echo
 
-read -p "which profile you want to use (use incus profile list for available profiles): " PROFILE
-echo
 
 
 # Hash the password for cloud init
@@ -114,7 +113,9 @@ CRYPTPASSWD=$(echo -n $PASSWD | mkpasswd -m sha-512 -R 4096 -S $PWSALT -s)
 # echo "inject cloud init user-data"
 export HOSTNAME=$HOSTNAME USERNAME=$USERNAME CRYPTPASSWD=$CRYPTPASSWD GITHUBNAME=$GITHUBNAME RELEASE=$OSVERSIONNAME
 
-CIDATA=$(cat $CLOUD_INIT | envsubst | yq ".users[].ssh_import_id = (load(\"${CDIR}/nodes/hardware_macs.yaml\").nodes[] | select(.name == \"${HOSTNAME}\" ).ssh-ids)" )
+# CIDATA=$(cat $CLOUD_INIT | envsubst | yq ".users[].ssh_import_id = (load(\"${CDIR}/nodes/hardware_macs.yaml\").nodes[] | select(.name == \"${HOSTNAME}\" ).ssh-ids)" )
+CIDATA=$(cat $CLOUD_INIT | envsubst )
+
 
 incus init -p $PROFILE ${OSNAME}/$OSVERSION/cloud $HOSTNAME
 echo "${CIDATA}" | incus config set $HOSTNAME user.user-data -
